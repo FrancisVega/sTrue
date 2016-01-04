2e5e4ea8e58a821a5a846a14fcb28d771ac64435
Francis <hisconer@gmail.com>
2016-01-04 17:51:30 +0100

    Add %shapes% query test

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
-sTrue("Plugin Testing", function() {
-	this.true(1 === "1"); // Fail!
-	this.class(context.selection[0], MSLayerGroup); // Pass!
+sTrue("Testeamos el query %shapes%", function() {
+
+  // @Sketch-Setup
+  // En el documento de sketch debe haber al menos una capa de tipo shape
+
+  // @Setup
+  var $shapesQuery = $('%shapes%');
+  var shapes = $shapesQuery.layers;
+
+  var arrayClass = [];
+  for(var i=0, len=shapes.length; i<len;i++) { arrayClass.push(shapes[i].class())	}
+  uniqueArray = arrayClass.filter(function(item, pos) { return arrayClass.indexOf(item) == pos; });
+
+  // @Tests	
+  this.true(uniqueArray.length === 1);
+  this.class(uniqueArray[0], "MSShapeGroup");
+  this.true($shapesQuery.length === shapes.length);
+  this.true(shapes.length > 0);
+
+  // @End
+  $shapesQuery = null;
+  shapes = null;
+
 });
